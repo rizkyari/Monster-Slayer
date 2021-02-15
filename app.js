e16bbacdbe8d176f606ff1291d36f77879047209
rizkyari <rizkyari4121@gmail.com>
2021-02-15 16:40:05 +0700

    health bar condition

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const app = Vue.createApp({
             monsterHealth : 100
         };
     },
+    computed:{
+        monsterBarStyle(){
+            return {
+                width: this.monsterHealth + '%'
+            };
+        },
+        playerBarStyle(){
+            return {
+                width: this.playerHealth + '%'
+            };
+        }
+    },
     methods: {
         attackMonster(){
             const attackValue = getRandomValue(5, 12);
